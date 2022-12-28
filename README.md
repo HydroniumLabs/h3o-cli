975b67c0950b7da95d8e7bca5844b5e9e1bd793b
Sylvain Laperche <sylvain.laperche@gmail.com>
2022-12-28 11:10:58 +0100

    add tests using lit
    First round of tests using lit to see if it's convenient or not.
    So far so good, will write more tests later.

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # h3o-cli — A CLI app exposing the h3o API for scripting
 
+[![Crates.io](https://img.shields.io/crates/v/h3o-cli.svg)](https://crates.io/crates/h3o-cli)
+[![Docs.rs](https://docs.rs/h3o-cli/badge.svg)](https://docs.rs/h3o-cli)
+[![CI Status](https://github.com/HydroniumLabs/h3o-cli/actions/workflows/ci.yml/badge.svg)](https://github.com/HydroniumLabs/h3o-cli/actions)
+[![License](https://img.shields.io/badge/license-BSD-green)](https://opensource.org/licenses/BSD-3-Clause)
+
 ## How to install
 
 ### Pre-compiled binaries
---
+++ tests/it.rs
@@ -0,0 +1,44 @@
+use std::{env, path::PathBuf};
+
+const CRATE_PATH: &str = env!("CARGO_MANIFEST_DIR");
+
+// From assert_cmd
+fn target_dir() -> PathBuf {
+    // Logic from `assert_cmd` crate.
+    env::current_exe()
+        .ok()
+        .map(|mut path| {
+            path.pop();
+            if path.ends_with("deps") {
+                path.pop();
+            }
+            path
+        })
+        .unwrap()
+}
+
+/// Look up the path to a cargo-built binary within an integration test.
+fn cargo_bin(name: &str) -> String {
+    // Logic from `assert_cmd` crate.
+    let env_var = format!("CARGO_BIN_EXE_{name}");
+    std::env::var_os(env_var)
+        .unwrap_or_else(|| {
+            target_dir()
+                .join(format!("{name}{}", env::consts::EXE_SUFFIX))
+                .into_os_string()
+        })
+        .into_string()
+        .expect("valid unicode path")
+}
+
+#[test]
+fn lit() {
+    lit::run::tests(lit::event_handler::Default::default(), |config| {
+        config.add_search_path(format!("{CRATE_PATH}/tests/lit"));
+        config.add_extension("txt");
+        config
+            .constants
+            .insert("cli".to_owned(), cargo_bin("h3o-cli"));
+    })
+    .expect("lit tests failed");
+}
