a25f04acb060f364e97c059e8713a9629ee4f836
Sylvain Laperche <sylvain.laperche@gmail.com>
2023-01-02 12:29:05 +0100

    remove unused code

--- src/json.rs
+++ src/json.rs
@@ -1,9 +1,6 @@
 use anyhow::{Context, Result as AnyResult};
-use serde::{
-    de::{self, Visitor},
-    Deserialize, Deserializer, Serialize, Serializer,
-};
-use std::{fmt, io};
+use serde::{Serialize, Serializer};
+use std::io;
 
 /// Output the value as JSON on `stdout`.
 ///
@@ -33,49 +30,11 @@ impl From<h3o::CellIndex> for CellIndex {
     }
 }
 
-impl From<CellIndex> for h3o::CellIndex {
-    fn from(value: CellIndex) -> Self {
-        value.0
-    }
-}
-
 impl Serialize for CellIndex {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
         serializer.serialize_str(&self.0.to_string())
-    }
-}
-
-impl<'de> Deserialize<'de> for CellIndex {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct CellIndexVisitor;
-
-        impl<'de> Visitor<'de> for CellIndexVisitor {
-            type Value = CellIndex;
-
-            fn expecting(
-                &self,
-                formatter: &mut fmt::Formatter<'_>,
-            ) -> fmt::Result {
-                formatter.write_str("the hexstring of a cell index")
-            }
-
-            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-            where
-                E: de::Error,
-            {
-                value
-                    .parse::<h3o::CellIndex>()
-                    .map(Into::into)
-                    .map_err(|err| E::custom(err.to_string()))
-            }
-        }
-
-        deserializer.deserialize_str(CellIndexVisitor)
     }
 }
